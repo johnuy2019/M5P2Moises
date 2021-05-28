dd93ae12fafde3e04de3a8abd14618e2d6a5a258
HP Omen <HP Omen@DESKTOP-NL61D4R>
2021-05-28 17:55:53 +0200

    Commit 3 P7 mover method quantitat

--- src/moises/Lloguer.java
+++ src/moises/Lloguer.java
@@ -35,5 +35,28 @@ public class Lloguer {
 	public void setVehicle(Vehicle vehicle) {
 	   this.vehicle = vehicle;
 	}
+	
+    public double quantitat() {
+    	double quantitat = 0;
+        switch (this.getVehicle().getCategoria()) {
+            case Vehicle.BASIC:
+                quantitat += 3;
+                if (this.getDies() > 3) {
+                    quantitat += (this.getDies() - 3) * 1.5;
+                }
+                break;
+            case Vehicle.GENERAL:
+                quantitat += 4;
+                if (this.getDies() > 2) {
+                    quantitat += (this.getDies() - 2) * 2.5;
+                }
+                break;
+            case Vehicle.LUXE:
+                quantitat += this.getDies() * 6;
+                break;
+             	
+        }
+        return quantitat;
+    }
 }
 	
